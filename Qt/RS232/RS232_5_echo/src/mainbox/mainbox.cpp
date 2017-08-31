85fe28828e92e5f931848022e5b441c95704e0c0
RinatB2017 <tux4096@gmail.com>
2017-09-01 00:00:25 +0300

    update

---
+++ Qt/RS232/RS232_5_echo/src/mainbox/mainbox.cpp
@@ -0,0 +1,125 @@
+/*********************************************************************************
+**                                                                              **
+**     Copyright (C) 2012                                                       **
+**                                                                              **
+**     This program is free software: you can redistribute it and/or modify     **
+**     it under the terms of the GNU General Public License as published by     **
+**     the Free Software Foundation, either version 3 of the License, or        **
+**     (at your option) any later version.                                      **
+**                                                                              **
+**     This program is distributed in the hope that it will be useful,          **
+**     but WITHOUT ANY WARRANTY; without even the implied warranty of           **
+**     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            **
+**     GNU General Public License for more details.                             **
+**                                                                              **
+**     You should have received a copy of the GNU General Public License        **
+**     along with this program.  If not, see http://www.gnu.org/licenses/.      **
+**                                                                              **
+**********************************************************************************
+**                   Author: Bikbao Rinat Zinorovich                            **
+**********************************************************************************/
+#include <QVBoxLayout>
+#include <QMessageBox>
+#include <QTime>
+
+#include <QAction>
+#include <QMenu>
+
+#include <QToolButton>
+#include <QToolBar>
+#include <QDebug>
+//--------------------------------------------------------------------------------
+#include "ui_mainbox.h"
+//--------------------------------------------------------------------------------
+#include "mysplashscreen.hpp"
+#include "mainwindow.hpp"
+#include "serialbox5.hpp"
+#include "mainbox.hpp"
+//--------------------------------------------------------------------------------
+MainBox::MainBox(QWidget *parent,
+                 MySplashScreen *splash) :
+    MyWidget(parent),
+    splash(splash),
+    ui(new Ui::MainBox),
+    serialBox5(0)
+{
+    init();
+}
+//--------------------------------------------------------------------------------
+MainBox::~MainBox()
+{
+    delete ui;
+}
+//--------------------------------------------------------------------------------
+void MainBox::init(void)
+{
+    ui->setupUi(this);
+
+    createTestBar();
+
+    serialBox5 = new SerialBox5(this, "RS232_5", "RS232");
+    serialBox5->add_menu(2);
+
+    ui->serial_layout->addWidget(serialBox5);
+
+    connect(serialBox5, SIGNAL(output(QByteArray)), serialBox5, SLOT(input(QByteArray)));
+}
+//--------------------------------------------------------------------------------
+QToolButton *MainBox::add_button(QToolBar *tool_bar,
+                                 QToolButton *tool_button,
+                                 QIcon icon,
+                                 const QString &text,
+                                 const QString &tool_tip)
+{
+    if(!tool_bar) return NULL;
+    if(!tool_button) return NULL;
+
+    tool_button->setIcon(icon);
+    tool_button->setText(text);
+    tool_button->setToolTip(tool_tip);
+    tool_bar->addWidget(tool_button);
+
+    return tool_button;
+}
+//--------------------------------------------------------------------------------
+void MainBox::createTestBar(void)
+{
+    QToolBar *toolBar = new QToolBar(tr("testbar"));
+    toolBar->setObjectName("testbar");
+
+    MainWindow *mw = dynamic_cast<MainWindow *>(parentWidget());
+    if(mw == nullptr)
+    {
+        return;
+    }
+
+    mw->addToolBar(Qt::TopToolBarArea, toolBar);
+
+    QToolButton *btn_test = add_button(toolBar,
+                                       new QToolButton(this),
+                                       qApp->style()->standardIcon(QStyle::SP_MediaPlay),
+                                       "test",
+                                       "test");
+
+    connect(btn_test, SIGNAL(clicked()), this, SLOT(test()));
+}
+//--------------------------------------------------------------------------------
+void MainBox::test(void)
+{
+    emit debug(tr("test"));
+}
+//--------------------------------------------------------------------------------
+void MainBox::changeEvent(QEvent *event)
+{
+    QWidget::changeEvent(event);
+    switch (event->type())
+    {
+    case QEvent::LanguageChange:
+        ui->retranslateUi(this);
+        break;
+
+    default:
+        break;
+    }
+}
+//--------------------------------------------------------------------------------

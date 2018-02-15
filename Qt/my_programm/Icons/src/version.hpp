f5da5730d4116d2571f98f15af13353c0a084750
RinatB2017 <tux4096@gmail.com>
2018-02-15 15:52:03 +0300

    update

--- Qt/my_programm/Icons/src/version.hpp
+++ Qt/my_programm/Icons/src/version.hpp
@@ -4,7 +4,7 @@
 #define VER_MAJOR 1
 #define VER_MINOR 3
 #define VER_PATCH 0
-#define VER_BUILD 815
-#define VER_STR "1.3.0.815"
+#define VER_BUILD 817
+#define VER_STR "1.3.0.817"
 //-----
 #endif // VERSION_HPP

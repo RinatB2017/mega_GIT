87ebb4759ec7d77031269288d8bd01261164e57c
RinatB2017 <tux4096@gmail.com>
2018-02-26 02:49:02 +0300

    update

--- Qt/my_programm/Organizations/Organization_03/RS485/RS485/src/main.cpp
+++ Qt/my_programm/Organizations/Organization_03/RS485/RS485/src/main.cpp
@@ -1,6 +1,6 @@
 /*********************************************************************************
 **                                                                              **
-**     Copyright (C) 2012                                                       **
+**     Copyright (C) 2018                                                       **
 **                                                                              **
 **     This program is free software: you can redistribute it and/or modify     **
 **     it under the terms of the GNU General Public License as published by     **
@@ -74,7 +74,7 @@ int main(int argc, char *argv[])
 
     splash->finish(main_window);
 
-#ifdef QT_DEBUG5
+#ifdef QT_DEBUG
     int test_result = QTest::qExec(new Test(), argc, argv);
     if (test_result != EXIT_SUCCESS)
     {

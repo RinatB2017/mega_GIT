e12782085f5b66d51ca40d857b2c72e06e1e1bc5
RinatB2017 <tux4096@gmail.com>
2018-04-20 21:48:00 +0300

    update

--- Qt/my_programm/_For_tests/src/version.hpp
+++ Qt/my_programm/_For_tests/src/version.hpp
@@ -4,7 +4,7 @@
 #define VER_MAJOR 0
 #define VER_MINOR 0
 #define VER_PATCH 1
-#define VER_BUILD 681
-#define VER_STR "0.0.1.681"
+#define VER_BUILD 682
+#define VER_STR "0.0.1.682"
 //-----
 #endif // VERSION_HPP

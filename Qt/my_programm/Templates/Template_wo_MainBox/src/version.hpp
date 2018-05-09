863728b22a51b99bc8a80e470e1aeac398933d66
RinatB2017 <tux4096@gmail.com>
2018-05-09 20:36:21 +0300

    update

--- Qt/my_programm/Templates/Template_wo_MainBox/src/version.hpp
+++ Qt/my_programm/Templates/Template_wo_MainBox/src/version.hpp
@@ -4,22 +4,7 @@
 #define VER_MAJOR 0
 #define VER_MINOR 0
 #define VER_PATCH 1
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 #define VER_BUILD 121
 #define VER_STR "0.0.1.121"
-=======
-#define VER_BUILD 27
-#define VER_STR "0.0.1.27"
->>>>>>> e12782085f5b66d51ca40d857b2c72e06e1e1bc5
-=======
-#define VER_BUILD 31
-#define VER_STR "0.0.1.31"
->>>>>>> ac1a98ae8eea13b416aea915be66ece93c199964
-=======
-#define VER_BUILD 33
-#define VER_STR "0.0.1.33"
->>>>>>> c40957fe0aef1a77e6fd8584783e531c79881e4d
 //-----
 #endif // VERSION_HPP

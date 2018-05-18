be958374cfeaa542a13277a6d9a5c5f23b07a32d
RinatB2017 <tux4096@gmail.com>
2018-05-18 16:03:00 +0300

    update

--- Qt/my_programm/Test_Polar/Test_Polar.pro
+++ Qt/my_programm/Test_Polar/Test_Polar.pro
@@ -3,65 +3,59 @@
 #**********************************************************************************
 
 TEMPLATE = app
-TARGET   = Teacher
+TARGET   = Test_Polar
+
+FOLDER  = tests
 
 DEPENDPATH  += \
     $$PWD \
     $$PWD/src \
-    $$PWD/src/teacher_mainbox \
-    $$PWD/src/teacher_mainbox/ui
+    $$PWD/src/test_polar \
+    $$PWD/src/test_polar/ui
 INCLUDEPATH = $$DEPENDPATH
-
-greaterThan(QT_MAJOR_VERSION, 4) {
-    QT += webkitwidgets
-} else {
-    QT += webkit
-}
-QT += network
 
 QMAKE_CXXFLAGS += -fno-show-column
 
 DEFINES += NO_STYLETOOLBAR
 DEFINES += NO_TRAYICON
-DEFINES += NO_LOG
-
-DEFINES += SAVE_WIDGETS_SPLITTER
-DEFINES += SAVE_WIDGETS_TEXTEDIT
-DEFINES += SAVE_WIDGETS_LINEEDIT
+DEFINES += NO_RESIZE
 
 DEFINES += PROGRAMM_IN_UTF8
-
-DEFINES += ULTRAWIDE
 
 HEADERS += \
     defines.hpp \
     version.hpp \
-    teacher_mainbox.hpp
+    test_polar.hpp
 
 SOURCES += \
-    teacher_mainbox.cpp \
+    test_polar.cpp \
     main.cpp
 
-FORMS   += teacher_mainbox.ui
-
-OTHER_FILES += doc/notebook.txt
-
-RESOURCES   += image/image.qrc
+FORMS   += test_polar.ui
 
 win32 {
     RC_FILE = ico/myapp.rc
 }
 
-LIB_PATH  = "../../lib"
-LIB_PATH2 = "../../lib2"
+# не забыть при смене Qt изменить файлы в каталоге win
+RESOURCES += \
+    images/images.qrc \
+    doc/doc.qrc
 
+OTHER_FILES += doc/notebook.txt
+
+CONFIG(debug, debug|release) {
+    include (src/test/test.pri)
+}
+
+#----------------------------------------------
+LIB_PATH = "../../lib"
 include ($$LIB_PATH/meta/mainwindow.pri)
-include (src/notebook/notebook.pri)
-include ($$LIB_PATH2/QXmlPutGet/qxmlputget.pri)
 
 !exists(OBJECTS_DIR) {
     VERSION_HEADER = src/version.hpp
     include ($$LIB_PATH/auto_inc_version.pri)
 }
+#----------------------------------------------
 
 VPATH = $$INCLUDEPATH
---
+++ Qt/my_programm/Test_Polar/src/test_polar/test_polar.cpp
@@ -0,0 +1,284 @@
+/*********************************************************************************
+**                                                                              **
+**     Copyright (C) 2015                                                       **
+**                                                                              **
+**     This program is free software: you can redistribute it and/or modify     **
+**     it under the terms of the GNU General Public License as published by     **
+**     the Free Software Foundation, either version 3 of the License, or        **
+**     (at your option) any later version.                                      **
+**                                                                              **
+**     This program is distributed in the hope that it will be useful,          **
+**     but WITHOUT ANY WARRANTY; without even the implied warranty of           **
+**     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            **
+**     GNU General Public License for more details.                             **
+**                                                                              **
+**     You should have received a copy of the GNU General Public License        **
+**     along with this program.  If not, see http://www.gnu.org/licenses/.      **
+**                                                                              **
+**********************************************************************************
+**                   Author: Bikbao Rinat Zinorovich                            **
+**********************************************************************************/
+#ifdef HAVE_QT5
+#   include <QtWidgets>
+#else
+#   include <QtGui>
+#endif
+//--------------------------------------------------------------------------------
+#include <QtMath>
+//--------------------------------------------------------------------------------
+#include "ui_test_polar.h"
+//--------------------------------------------------------------------------------
+#include "mywaitsplashscreen.hpp"
+#include "mysplashscreen.hpp"
+#include "mainwindow.hpp"
+#include "test_polar.hpp"
+#include "defines.hpp"
+//--------------------------------------------------------------------------------
+#ifdef QT_DEBUG
+#   include <QDebug>
+#endif
+//--------------------------------------------------------------------------------
+MainBox::MainBox(QWidget *parent,
+                 MySplashScreen *splash) :
+    MyWidget(parent),
+    splash(splash),
+    ui(new Ui::MainBox)
+{
+    init();
+}
+//--------------------------------------------------------------------------------
+MainBox::~MainBox()
+{
+    delete ui;
+}
+//--------------------------------------------------------------------------------
+void MainBox::init(void)
+{
+    ui->setupUi(this);
+
+    createTestBar();
+    installEventFilter(this);
+
+    setFixedSize(LEN_SIDE+1, LEN_SIDE+1);
+}
+//--------------------------------------------------------------------------------
+void MainBox::createTestBar(void)
+{
+    MainWindow *mw = dynamic_cast<MainWindow *>(topLevelWidget());
+    Q_CHECK_PTR(mw);
+
+    commands.clear();
+    commands.append({ ID_TEST_0, "test 0", &MainBox::test_0 });
+    commands.append({ ID_TEST_1, "test 1", &MainBox::test_1 });
+    commands.append({ ID_TEST_2, "test 2", &MainBox::test_2 });
+    commands.append({ ID_TEST_3, "test 3", &MainBox::test_3 });
+    commands.append({ ID_TEST_4, "test 4", &MainBox::test_4 });
+    commands.append({ ID_TEST_5, "test 5", &MainBox::test_5 });
+
+    QToolBar *testbar = new QToolBar("testbar");
+    testbar->setObjectName("testbar");
+    mw->addToolBar(Qt::TopToolBarArea, testbar);
+
+    cb_block = new QCheckBox("block", this);
+    testbar->addWidget(cb_block);
+
+    cb_test = new QComboBox(this);
+    cb_test->setObjectName("cb_test");
+    foreach (CMD command, commands)
+    {
+        cb_test->addItem(command.cmd_text, QVariant(command.cmd));
+    }
+
+    testbar->addWidget(cb_test);
+    QToolButton *btn_choice_test = add_button(testbar,
+                                              new QToolButton(this),
+                                              qApp->style()->standardIcon(QStyle::SP_MediaPlay),
+                                              "choice_test",
+                                              "choice_test");
+    btn_choice_test->setObjectName("btn_choice_test");
+
+    connect(btn_choice_test, SIGNAL(clicked()), this, SLOT(choice_test()));
+
+    connect(cb_block, SIGNAL(clicked(bool)), cb_test,           SLOT(setDisabled(bool)));
+    connect(cb_block, SIGNAL(clicked(bool)), btn_choice_test,   SLOT(setDisabled(bool)));
+
+    //testbar->setFixedWidth(toolBar->sizeHint().width());
+}
+//--------------------------------------------------------------------------------
+void MainBox::choice_test(void)
+{
+    bool ok = false;
+    int cmd = cb_test->itemData(cb_test->currentIndex(), Qt::UserRole).toInt(&ok);
+    if(!ok)
+    {
+        return;
+    }
+    foreach (CMD command, commands)
+    {
+        if(command.cmd == cmd)
+        {
+            typedef bool (MainBox::*my_mega_function)(void);
+            my_mega_function x;
+            x = command.func;
+            if(x)
+            {
+                (this->*x)();
+            }
+            else
+            {
+                emit error("no func");
+            }
+
+            return;
+        }
+    }
+}
+//--------------------------------------------------------------------------------
+bool MainBox::test_0(void)
+{
+    emit info("Test_0()");
+
+    qreal len_big_circle = M_PI * (double)LEN_SIDE;
+    qreal len_small_circle = M_PI * 2 * SMALL_R;
+
+    emit info(QString("Длина большой окружности %1 pix").arg(len_big_circle));
+    emit info(QString("Длина малой окружности %1 pix").arg(len_small_circle));
+    emit info(QString("Отношения длин %1").arg(len_big_circle / len_small_circle));
+    emit info(QString("cnt_point %1").arg(cnt_point));
+
+    return true;
+}
+//--------------------------------------------------------------------------------
+bool MainBox::test_1(void)
+{
+    emit info("Test_1()");
+
+    QPixmap pixmap = grab(QRect(0, 0, LEN_SIDE, LEN_SIDE));
+    QImage image = pixmap.toImage();
+
+    qreal min_r = SMALL_R;
+    qreal max_r = (LEN_SIDE / 2);
+
+    qreal height = max_r - min_r;
+    qreal width  = M_PI * (double)LEN_SIDE;
+
+    emit info(QString("min_r %1").arg(min_r));
+    emit info(QString("max_r %1").arg(max_r));
+    emit info(QString("width %1").arg(width));
+    emit info(QString("height %1").arg(height));
+
+    QImage *res_image = new QImage(width, height, QImage::Format_RGB32);
+    res_image->fill(Qt::white);
+
+#if 0
+    for(int x=0; x<LEN_SIDE; x++)
+    {
+        for(int y=0; y<LEN_SIDE; y++)
+        {
+             QRgb rgb = image.pixel(x, y);
+             res_image->setPixel(x, y, rgb);
+        }
+    }
+#endif
+
+    QLabel *label = new QLabel;
+    label->setFixedSize(res_image->width(),
+                        res_image->height());
+    label->setPixmap(QPixmap::fromImage(*res_image));
+    label->show();
+
+    //bool ok = res_image->save("./test_pixel.png");
+    //emit info(QString("save return %1").arg(ok ? "true" : "false"));
+
+    emit info("OK");
+    return true;
+}
+//--------------------------------------------------------------------------------
+bool MainBox::test_2(void)
+{
+    emit info("Test_2()");
+
+    return true;
+}
+//--------------------------------------------------------------------------------
+bool MainBox::test_3(void)
+{
+    emit info("Test_3()");
+
+    return true;
+}
+//--------------------------------------------------------------------------------
+bool MainBox::test_4(void)
+{
+    emit info("Test_4()");
+
+    return true;
+}
+//--------------------------------------------------------------------------------
+bool MainBox::test_5(void)
+{
+    emit info("Test_5()");
+
+    return true;
+}
+//--------------------------------------------------------------------------------
+void MainBox::updateText(void)
+{
+    ui->retranslateUi(this);
+}
+//--------------------------------------------------------------------------------
+void MainBox::paintEvent(QPaintEvent *)
+{
+    QPainter p(this);
+    QPoint center;
+    center.setX(LEN_SIDE/2);
+    center.setY(LEN_SIDE/2);
+
+    p.setPen(QPen(Qt::red, 1, Qt::SolidLine));
+    p.drawEllipse(0, 0, LEN_SIDE, LEN_SIDE);
+
+    p.drawEllipse(center, SMALL_R, SMALL_R);
+
+    p.drawLine(0, 0, LEN_SIDE, LEN_SIDE);
+    p.drawLine(LEN_SIDE/2, 0, LEN_SIDE/2, LEN_SIDE);
+    p.drawLine(LEN_SIDE, 0, 0, LEN_SIDE);
+    p.drawLine(0, LEN_SIDE/2, LEN_SIDE, LEN_SIDE/2);
+
+    QPoint center_circle_1;
+    QPoint center_circle_2;
+    QPoint center_circle_3;
+    QPoint center_circle_4;
+
+    center_circle_1.setX(LEN_SIDE / 4);
+    center_circle_1.setY(LEN_SIDE / 4);
+
+    center_circle_2.setX(LEN_SIDE / 2 + LEN_SIDE / 4);
+    center_circle_2.setY(LEN_SIDE / 4);
+
+    center_circle_3.setX(LEN_SIDE / 2 + LEN_SIDE / 4);
+    center_circle_3.setY(LEN_SIDE / 2 + LEN_SIDE / 4);
+
+    center_circle_4.setX(LEN_SIDE / 4);
+    center_circle_4.setY(LEN_SIDE / 2 + LEN_SIDE / 4);
+
+    p.drawEllipse(center_circle_1, 50, 50);
+    p.drawEllipse(center_circle_2, 60, 60);
+    p.drawEllipse(center_circle_3, 70, 70);
+    p.drawEllipse(center_circle_4, 80, 80);
+
+#if 0
+    cnt_point = 0;
+    for(qreal angle=0; angle<90; angle+=0.01)
+    {
+        for(int radius=SMALL_R; radius<(LEN_SIDE / 2); radius++)
+        {
+            qreal x = radius * qCos(qDegreesToRadians((qreal)angle));
+            qreal y = radius * qSin(qDegreesToRadians((qreal)angle));
+            p.drawPoint(x + center.x(),
+                        y + center.y());
+            cnt_point++;
+        }
+    }
+#endif
+}
+//--------------------------------------------------------------------------------

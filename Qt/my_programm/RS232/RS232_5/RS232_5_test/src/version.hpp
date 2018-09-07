58e72021484fe938a8707b104ae641c0d8019d80
RinatB2017 <tux4096@gmail.com>
2018-09-07 23:10:47 +0300

    update

--- Qt/my_programm/RS232/RS232_5/RS232_5_test/src/version.hpp
+++ Qt/my_programm/RS232/RS232_5/RS232_5_test/src/version.hpp
@@ -4,7 +4,7 @@
 #define VER_MAJOR 0
 #define VER_MINOR 99
 #define VER_PATCH 0
-#define VER_BUILD 394
-#define VER_STR "0.99.0.394"
+#define VER_BUILD 395
+#define VER_STR "0.99.0.395"
 //-----
 #endif // VERSION_HPP

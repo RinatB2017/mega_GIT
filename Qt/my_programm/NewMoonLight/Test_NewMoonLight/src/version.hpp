361b99b06f956462159d082970879324c23e224f
RinatB2017 <tux4096@gmail.com>
2018-09-08 01:29:20 +0300

    update

--- Qt/my_programm/NewMoonLight/Test_NewMoonLight/src/version.hpp
+++ Qt/my_programm/NewMoonLight/Test_NewMoonLight/src/version.hpp
@@ -4,7 +4,7 @@
 #define VER_MAJOR 0
 #define VER_MINOR 1
 #define VER_PATCH 0
-#define VER_BUILD 263
-#define VER_STR "0.1.0.263"
+#define VER_BUILD 264
+#define VER_STR "0.1.0.264"
 //-----
 #endif // VERSION_HPP

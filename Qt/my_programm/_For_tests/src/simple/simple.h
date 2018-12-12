ef5bf79e55cefa9e6ce52783c073e6d1ea608b88
RinatB2017 <tux4096@gmail.com>
2018-12-12 22:21:58 +0300

    update

--- Qt/my_programm/_For_tests/src/simple/simple.h
+++ Qt/my_programm/_For_tests/src/simple/simple.h
@@ -1,4 +1,8 @@
 
+#if 0
 extern "C" {
     int simple_mul(int value);
 }
+#else
+    extern "C" int simple_mul(int value);
+#endif
--- Qt/my_programm/_For_tests/src/version.hpp
+++ Qt/my_programm/_For_tests/src/version.hpp
@@ -4,7 +4,7 @@
 #define VER_MAJOR 0
 #define VER_MINOR 0
 #define VER_PATCH 1
-#define VER_BUILD 941
-#define VER_STR "0.0.1.941"
+#define VER_BUILD 942
+#define VER_STR "0.0.1.942"
 //-----
 #endif // VERSION_HPP

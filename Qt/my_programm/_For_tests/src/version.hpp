21a86a6ae1fa20ddd3b2114c4c9b58917ce6d920
RinatB2017 <tux4096@gmail.com>
2018-12-24 14:17:12 +0300

    update

--- Qt/my_programm/_For_tests/src/version.hpp
+++ Qt/my_programm/_For_tests/src/version.hpp
@@ -4,7 +4,7 @@
 #define VER_MAJOR 0
 #define VER_MINOR 0
 #define VER_PATCH 1
-#define VER_BUILD 948
-#define VER_STR "0.0.1.948"
+#define VER_BUILD 949
+#define VER_STR "0.0.1.949"
 //-----
 #endif // VERSION_HPP
--- Qt/tests/Test_HTML/src/version.hpp
+++ Qt/tests/Test_HTML/src/version.hpp
@@ -4,7 +4,7 @@
 #define VER_MAJOR 0
 #define VER_MINOR 0
 #define VER_PATCH 1
-#define VER_BUILD 28
-#define VER_STR "0.0.1.28"
+#define VER_BUILD 29
+#define VER_STR "0.0.1.29"
 //-----
 #endif // VERSION_HPP

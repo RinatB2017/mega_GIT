e01c94f30c3cd6d00ff7088aaf3ed14216195e06
RinatB2017 <tux4096@gmail.com>
2019-01-21 19:09:32 +0300

    update

--- Qt/tests/Test_QWebEngineView/src/version.hpp
+++ Qt/tests/Test_QWebEngineView/src/version.hpp
@@ -4,7 +4,7 @@
 #define VER_MAJOR 1
 #define VER_MINOR 0
 #define VER_PATCH 0
-#define VER_BUILD 69
-#define VER_STR "1.0.0.69"
+#define VER_BUILD 70
+#define VER_STR "1.0.0.70"
 //-----
 #endif // VERSION_HPP

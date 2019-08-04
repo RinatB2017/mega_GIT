aa465bef52521944e4d81f58d0cc5227e9e2f324
RinatB2017 <tux4096@gmail.com>
2019-08-04 23:36:39 +0300

    update

--- Qt/my_programm/_For_tests/src/version.hpp
+++ Qt/my_programm/_For_tests/src/version.hpp
@@ -4,7 +4,7 @@
 #define VER_MAJOR 0
 #define VER_MINOR 0
 #define VER_PATCH 1
-#define VER_BUILD 1109
-#define VER_STR "0.0.1.1109"
+#define VER_BUILD 1110
+#define VER_STR "0.0.1.1110"
 //-----
 #endif // VERSION_HPP

e08c9ddab43f6718c091d84d78f341337ee18f73
Tux4096 <tux4096@gmail.com>
2019-11-18 16:42:40 +0300

    update

--- Qt/lib2/widgets/rtsp_widget/src/rtsp_widget.cpp
+++ Qt/lib2/widgets/rtsp_widget/src/rtsp_widget.cpp
@@ -61,7 +61,8 @@ void RTSP_widget::init(void)
     ui->le_address->setText("rtsp://admin:admin@192.168.1.11/0/video0");
 #else
     //ui->le_address->setText("rtsp://192.168.1.88/HD");
-    ui->le_address->setText("rtsp://192.168.10.101:8001/0/video0");
+    //ui->le_address->setText("rtsp://192.168.10.101:8001/0/video0");
+    ui->le_address->setText("rtsp://192.168.10.159");
 #endif
     ui->le_address->setEnabled(false);
 

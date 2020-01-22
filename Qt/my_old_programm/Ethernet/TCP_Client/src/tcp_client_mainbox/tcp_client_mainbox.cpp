35087336ab98d369dc43eaee3db8aa2a25f5a0e9
Tux4096 <tux4096@gmail.com>
2020-01-22 15:21:02 +0300

    update

--- Qt/my_old_programm/Ethernet/TCP_Client/src/tcp_client_mainbox/tcp_client_mainbox.cpp
+++ Qt/my_old_programm/Ethernet/TCP_Client/src/tcp_client_mainbox/tcp_client_mainbox.cpp
@@ -107,9 +107,12 @@ void MainBox::send(void)
 
     data.clear();
     data.append(ui->le_data->text());
-    emit debug(QString("send(%1)").arg(data.data()));
+    //data.append(QByteArray::fromHex("A0 01 01 A2"));
+    //data.append(QByteArray::fromHex("A0 01 00 A1"));
+
+    //emit debug(QString("send(%1)").arg(data.data()));
     res_data = client->input(data);
-    //emit debug(res_data.toHex());
+    emit debug(res_data.toHex());
     emit info(res_data);
 }
 //--------------------------------------------------------------------------------

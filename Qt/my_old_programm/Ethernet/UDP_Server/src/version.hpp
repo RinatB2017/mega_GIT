ef0e5b7d24d5062a2122f31e7e83fe2f2caec5aa
RinatB2017 <tux4096@gmail.com>
2020-01-24 20:21:50 +0300

    update

--- Qt/my_old_programm/Ethernet/UDP_Server/src/version.hpp
+++ Qt/my_old_programm/Ethernet/UDP_Server/src/version.hpp
@@ -4,7 +4,7 @@
 #define VER_MAJOR 0
 #define VER_MINOR 1
 #define VER_PATCH 0
-#define VER_BUILD 397
-#define VER_STR "0.1.0.397"
+#define VER_BUILD 398
+#define VER_STR "0.1.0.398"
 //-----
 #endif // VERSION_HPP

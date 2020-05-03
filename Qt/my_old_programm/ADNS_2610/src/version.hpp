78e001e1d906d0e88cb06fbf89fb49695ec93fa5
RinatB2017 <tux4096@gmail.com>
2020-05-03 23:24:46 +0300

    update

--- Qt/my_old_programm/ADNS_2610/src/version.hpp
+++ Qt/my_old_programm/ADNS_2610/src/version.hpp
@@ -4,7 +4,7 @@
 #define VER_MAJOR 0
 #define VER_MINOR 0
 #define VER_PATCH 0
-#define VER_BUILD 921
-#define VER_STR "0.0.0.921"
+#define VER_BUILD 922
+#define VER_STR "0.0.0.922"
 //-----
 #endif // VERSION_HPP
--- Qt/my_programm/Teacher/src/version.hpp
+++ Qt/my_programm/Teacher/src/version.hpp
@@ -4,7 +4,7 @@
 #define VER_MAJOR 0
 #define VER_MINOR 5
 #define VER_PATCH 0
-#define VER_BUILD 528
-#define VER_STR "0.5.0.528"
+#define VER_BUILD 529
+#define VER_STR "0.5.0.529"
 //-----
 #endif // VERSION_HPP

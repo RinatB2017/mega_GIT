9a7dc8ad0edc437411696625fd0c078fc3002809
tux4096@gmail.com <tux4096@gmail.com>
2020-06-28 22:06:54 +0300

    update

--- Qt/tests/Test_QWebEngineView/src/version.hpp
+++ Qt/tests/Test_QWebEngineView/src/version.hpp
@@ -4,7 +4,7 @@
 #define VER_MAJOR 1
 #define VER_MINOR 0
 #define VER_PATCH 0
-#define VER_BUILD 241
-#define VER_STR "1.0.0.241"
+#define VER_BUILD 244
+#define VER_STR "1.0.0.244"
 //-----
 #endif // VERSION_HPP

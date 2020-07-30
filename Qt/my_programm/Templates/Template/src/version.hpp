f85545cbe67c56012b4cccb263ac26d885128ba2
tux4096@gmail.com <tux4096@gmail.com>
2020-07-30 12:06:49 +0300

    update

--- Qt/my_programm/Templates/Template/src/version.hpp
+++ Qt/my_programm/Templates/Template/src/version.hpp
@@ -4,7 +4,7 @@
 #define VER_MAJOR 0
 #define VER_MINOR 0
 #define VER_PATCH 1
-#define VER_BUILD 453
-#define VER_STR "0.0.1.453"
+#define VER_BUILD 454
+#define VER_STR "0.0.1.454"
 //-----
 #endif // VERSION_HPP

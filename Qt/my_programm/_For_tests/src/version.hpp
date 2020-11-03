e40b1dbfd415aae6c209eaf49ea59d4f49d245a7
tux4096@gmail.com <tux4096@gmail.com>
2020-11-03 22:29:21 +0300

    update

--- Qt/my_programm/_For_tests/src/version.hpp
+++ Qt/my_programm/_For_tests/src/version.hpp
@@ -4,7 +4,7 @@
 #define VER_MAJOR 1
 #define VER_MINOR 0
 #define VER_PATCH 0
-#define VER_BUILD 4
-#define VER_STR "1.0.0.4"
+#define VER_BUILD 6
+#define VER_STR "1.0.0.6"
 //-----
 #endif // VERSION_HPP

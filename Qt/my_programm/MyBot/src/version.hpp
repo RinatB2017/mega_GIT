a798ded2e6f319b286a3795be2c83f483395908b
tux4096@gmail.com <tux4096@gmail.com>
2020-12-14 14:27:16 +0300

    update

--- Qt/my_programm/MyBot/src/version.hpp
+++ Qt/my_programm/MyBot/src/version.hpp
@@ -4,7 +4,7 @@
 #define VER_MAJOR 0
 #define VER_MINOR 0
 #define VER_PATCH 1
-#define VER_BUILD 579
-#define VER_STR "0.0.1.579"
+#define VER_BUILD 580
+#define VER_STR "0.0.1.580"
 //-----
 #endif // VERSION_HPP

54282369de081fc43bf55765406e22f8febac860
tux4096@gmail.com <tux4096@gmail.com>
2022-01-09 17:46:31 +0300

    update

--- Qt/my_programm/_For_tests/src/for_tests_mainbox/src/for_tests_mainbox.cpp
+++ Qt/my_programm/_For_tests/src/for_tests_mainbox/src/for_tests_mainbox.cpp
@@ -232,6 +232,25 @@ bool MainBox::test(void)
     emit trace(Q_FUNC_INFO);
 
 #if 1
+    int r = 150;
+    int g = 150;
+    int b = 150;
+
+    int frame_width  = 1920;
+    int frame_height = 1080;
+
+    QPixmap *pixmap = new QPixmap(frame_width, frame_height);
+    QPainter painter;
+    painter.begin(pixmap);
+    painter.fillRect(0, 0, frame_width, frame_height, QBrush(QColor(r, g, b)));
+    painter.end();
+
+    QLabel *label = new QLabel();
+    label->setPixmap(*pixmap);
+    label->show();
+#endif
+
+#if 0
     TestWidget *tw = new TestWidget();
     tw->show();
 #endif

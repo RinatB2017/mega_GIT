377ed91443a3b08e0fd27ba7fefe1714562c395c
RinatB2017 <tux4096@gmail.com>
2026-01-19 16:01:06 +0300

    update

---
+++ Qt/my_programm/MODBUS/serialbus/can/ui_mainwindow.h
@@ -0,0 +1,183 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.10.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+#include <sendframebox.h>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionConnect;
+    QAction *actionDisconnect;
+    QAction *actionQuit;
+    QAction *actionAboutQt;
+    QAction *actionClearLog;
+    QAction *actionPluginDocumentation;
+    QWidget *centralWidget;
+    QVBoxLayout *verticalLayout;
+    SendFrameBox *sendFrameBox;
+    QGroupBox *receivedMessagesBox;
+    QVBoxLayout *verticalLayout_4;
+    QLabel *label_3;
+    QTextEdit *receivedMessagesEdit;
+    QMenuBar *menuBar;
+    QMenu *menuCalls;
+    QMenu *menuHelp;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(551, 481);
+        actionConnect = new QAction(MainWindow);
+        actionConnect->setObjectName("actionConnect");
+        QIcon icon;
+        icon.addFile(QString::fromUtf8(":/images/connect.png"), QSize(), QIcon::Mode::Normal, QIcon::State::Off);
+        actionConnect->setIcon(icon);
+        actionDisconnect = new QAction(MainWindow);
+        actionDisconnect->setObjectName("actionDisconnect");
+        QIcon icon1;
+        icon1.addFile(QString::fromUtf8(":/images/disconnect.png"), QSize(), QIcon::Mode::Normal, QIcon::State::Off);
+        actionDisconnect->setIcon(icon1);
+        actionQuit = new QAction(MainWindow);
+        actionQuit->setObjectName("actionQuit");
+        QIcon icon2;
+        icon2.addFile(QString::fromUtf8(":/images/application-exit.png"), QSize(), QIcon::Mode::Normal, QIcon::State::Off);
+        actionQuit->setIcon(icon2);
+        actionAboutQt = new QAction(MainWindow);
+        actionAboutQt->setObjectName("actionAboutQt");
+        actionClearLog = new QAction(MainWindow);
+        actionClearLog->setObjectName("actionClearLog");
+        QIcon icon3;
+        icon3.addFile(QString::fromUtf8(":/images/clear.png"), QSize(), QIcon::Mode::Normal, QIcon::State::Off);
+        actionClearLog->setIcon(icon3);
+        actionPluginDocumentation = new QAction(MainWindow);
+        actionPluginDocumentation->setObjectName("actionPluginDocumentation");
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName("centralWidget");
+        verticalLayout = new QVBoxLayout(centralWidget);
+        verticalLayout->setSpacing(6);
+        verticalLayout->setContentsMargins(11, 11, 11, 11);
+        verticalLayout->setObjectName("verticalLayout");
+        sendFrameBox = new SendFrameBox(centralWidget);
+        sendFrameBox->setObjectName("sendFrameBox");
+
+        verticalLayout->addWidget(sendFrameBox);
+
+        receivedMessagesBox = new QGroupBox(centralWidget);
+        receivedMessagesBox->setObjectName("receivedMessagesBox");
+        verticalLayout_4 = new QVBoxLayout(receivedMessagesBox);
+        verticalLayout_4->setSpacing(6);
+        verticalLayout_4->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_4->setObjectName("verticalLayout_4");
+        label_3 = new QLabel(receivedMessagesBox);
+        label_3->setObjectName("label_3");
+        QFont font;
+        font.setFamilies({QString::fromUtf8("Courier")});
+        font.setKerning(false);
+        label_3->setFont(font);
+        label_3->setFrameShape(QFrame::StyledPanel);
+
+        verticalLayout_4->addWidget(label_3);
+
+        receivedMessagesEdit = new QTextEdit(receivedMessagesBox);
+        receivedMessagesEdit->setObjectName("receivedMessagesEdit");
+        receivedMessagesEdit->setFont(font);
+        receivedMessagesEdit->setFrameShape(QFrame::StyledPanel);
+        receivedMessagesEdit->setUndoRedoEnabled(false);
+        receivedMessagesEdit->setLineWrapMode(QTextEdit::NoWrap);
+        receivedMessagesEdit->setReadOnly(true);
+
+        verticalLayout_4->addWidget(receivedMessagesEdit);
+
+
+        verticalLayout->addWidget(receivedMessagesBox);
+
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName("menuBar");
+        menuBar->setGeometry(QRect(0, 0, 551, 19));
+        menuCalls = new QMenu(menuBar);
+        menuCalls->setObjectName("menuCalls");
+        menuHelp = new QMenu(menuBar);
+        menuHelp->setObjectName("menuHelp");
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName("mainToolBar");
+        MainWindow->addToolBar(Qt::ToolBarArea::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName("statusBar");
+        MainWindow->setStatusBar(statusBar);
+
+        menuBar->addAction(menuCalls->menuAction());
+        menuBar->addAction(menuHelp->menuAction());
+        menuCalls->addAction(actionConnect);
+        menuCalls->addAction(actionDisconnect);
+        menuCalls->addSeparator();
+        menuCalls->addAction(actionClearLog);
+        menuCalls->addSeparator();
+        menuCalls->addAction(actionQuit);
+        menuHelp->addAction(actionPluginDocumentation);
+        menuHelp->addAction(actionAboutQt);
+        mainToolBar->addAction(actionConnect);
+        mainToolBar->addAction(actionDisconnect);
+        mainToolBar->addSeparator();
+        mainToolBar->addAction(actionClearLog);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "CAN Example", nullptr));
+        actionConnect->setText(QCoreApplication::translate("MainWindow", "&Connect", nullptr));
+        actionDisconnect->setText(QCoreApplication::translate("MainWindow", "&Disconnect", nullptr));
+        actionQuit->setText(QCoreApplication::translate("MainWindow", "&Quit", nullptr));
+        actionAboutQt->setText(QCoreApplication::translate("MainWindow", "&About Qt", nullptr));
+        actionClearLog->setText(QCoreApplication::translate("MainWindow", "Clear &Log", nullptr));
+        actionPluginDocumentation->setText(QCoreApplication::translate("MainWindow", "Plugin Documentation", nullptr));
+#if QT_CONFIG(tooltip)
+        actionPluginDocumentation->setToolTip(QCoreApplication::translate("MainWindow", "Open plugin documentation in Webbrowser", nullptr));
+#endif // QT_CONFIG(tooltip)
+        sendFrameBox->setTitle(QCoreApplication::translate("MainWindow", "Send CAN frame", nullptr));
+        receivedMessagesBox->setTitle(QCoreApplication::translate("MainWindow", "Received CAN messages", nullptr));
+        label_3->setText(QCoreApplication::translate("MainWindow", "Timestamp        Flags  CAN-ID   DLC  Data", nullptr));
+        menuCalls->setTitle(QCoreApplication::translate("MainWindow", "&Calls", nullptr));
+        menuHelp->setTitle(QCoreApplication::translate("MainWindow", "&Help", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
---
+++ Qt/my_programm/MODBUS/serialbus/modbus/adueditor/moc_mainwindow.cpp
@@ -0,0 +1,124 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 69 (Qt 6.10.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "src/mainwindow.h"
+#include <QtGui/qtextcursor.h>
+#include <QtCore/qmetatype.h>
+
+#include <QtCore/qtmochelpers.h>
+
+#include <memory>
+
+
+#include <QtCore/qxptype_traits.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 69
+#error "This file was generated using the moc from 6.10.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+struct qt_meta_tag_ZN10MainWindowE_t {};
+} // unnamed namespace
+
+template <> constexpr inline auto MainWindow::qt_create_metaobjectdata<qt_meta_tag_ZN10MainWindowE_t>()
+{
+    namespace QMC = QtMocConstants;
+    QtMocHelpers::StringRefStorage qt_stringData {
+        "MainWindow",
+        "on_sendButton_clicked",
+        "",
+        "on_connectButton_clicked",
+        "on_disconnectButton_clicked",
+        "on_serialPortCombo_currentIndexChanged",
+        "index"
+    };
+
+    QtMocHelpers::UintData qt_methods {
+        // Slot 'on_sendButton_clicked'
+        QtMocHelpers::SlotData<void()>(1, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'on_connectButton_clicked'
+        QtMocHelpers::SlotData<void()>(3, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'on_disconnectButton_clicked'
+        QtMocHelpers::SlotData<void()>(4, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'on_serialPortCombo_currentIndexChanged'
+        QtMocHelpers::SlotData<void(int)>(5, 2, QMC::AccessPrivate, QMetaType::Void, {{
+            { QMetaType::Int, 6 },
+        }}),
+    };
+    QtMocHelpers::UintData qt_properties {
+    };
+    QtMocHelpers::UintData qt_enums {
+    };
+    return QtMocHelpers::metaObjectData<MainWindow, qt_meta_tag_ZN10MainWindowE_t>(QMC::MetaObjectFlag{}, qt_stringData,
+            qt_methods, qt_properties, qt_enums);
+}
+Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_staticMetaObjectStaticContent<qt_meta_tag_ZN10MainWindowE_t>.stringdata,
+    qt_staticMetaObjectStaticContent<qt_meta_tag_ZN10MainWindowE_t>.data,
+    qt_static_metacall,
+    nullptr,
+    qt_staticMetaObjectRelocatingContent<qt_meta_tag_ZN10MainWindowE_t>.metaTypes,
+    nullptr
+} };
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    auto *_t = static_cast<MainWindow *>(_o);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        switch (_id) {
+        case 0: _t->on_sendButton_clicked(); break;
+        case 1: _t->on_connectButton_clicked(); break;
+        case 2: _t->on_disconnectButton_clicked(); break;
+        case 3: _t->on_serialPortCombo_currentIndexChanged((*reinterpret_cast<std::add_pointer_t<int>>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_staticMetaObjectStaticContent<qt_meta_tag_ZN10MainWindowE_t>.strings))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 4)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 4;
+    }
+    if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 4)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 4;
+    }
+    return _id;
+}
+QT_WARNING_POP

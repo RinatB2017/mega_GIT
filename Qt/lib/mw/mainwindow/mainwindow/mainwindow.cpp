225b53f98ee7785c69f4b2347baffc1c7a34196d
RinatB <tux4096@gmail.com>
2026-09-17 01:02:42 +0300

    update

--- Qt/lib/mw/mainwindow/mainwindow/mainwindow.cpp
+++ Qt/lib/mw/mainwindow/mainwindow/mainwindow.cpp
@@ -20,7 +20,7 @@
 **********************************************************************************/
 #include "mainwindow.hpp"
 //--------------------------------------------------------------------------------
-#ifdef QT_DEBUG
+#ifdef BAD_TEST
 #include <sanitizer/asan_interface.h>
 
 // ASan автоматически вызовет эту функцию до функции main()
@@ -28,7 +28,6 @@ extern "C" const char* __asan_default_options() {
     return "abort_on_error=1:detect_leaks=0";
     // return "abort_on_error=1";
 }
-
 #endif
 //--------------------------------------------------------------------------------
 MainWindow::MainWindow(QWidget *parent) :
